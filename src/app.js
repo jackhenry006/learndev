d8837fc1c21f7900339aaebdb4326b3e82da0a72
Ankit Kumar Nayak <ankitkumarnayak006@gmail.com>
2025-12-02 23:16:01 +0530

    dynamic api

--- src/app.js
+++ src/app.js
@@ -3,18 +3,31 @@ const app = express();
 const connectDB = require("./config/database.js");
 const User = require("./models/user.js");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const userobj = {
-    firstName: "Amit",
-    lastName: "Kumar Nayak",
-    emailId: "Amit@gamil.com",
-    password: "Amit@123",
-  };
+  // console.log(req.body);
+  const userobj = req.body;
   try {
     const user = new User(userobj);
 
     await user.save();
     res.send("User added sucessfully in the database");
+  } catch (err) {
+    res.status(404).send("something went wrong " + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
   } catch (err) {
     res.status(404).send("something went wrong " + err.message);
   }
